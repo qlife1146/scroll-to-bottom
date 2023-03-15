0e9278224aa105dc144a877282df24ab82524280
Luca Park <qlife1146@gmail.com>
2023-03-15 15:34:33 +0900

    init

---
+++ scroll-to-bottom.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name		Scroll to Bottom Button
+// @version		1.0.0
+// @description	페이지 아래로 바로 내려갈 수 있는 버튼을 추가합니다.
+// @author		Luca_Tuna
+// @icon		http://vocaro.wikidot.com/local--files/nav:side/logo.png
+// @grant		none
+// @match		http://vocaro.wikidot.com/*
+// ==/UserScript==
+
+
+(function () {
+    "use strict";
+
+    const button = document.createElement("button");
+    button.textContent = "맨 아래로";
+    button.onclick = scrollToBottom;
+    button.style.position = 'fixed';
+    button.style.bottom = '0';
+    button.style.right = '0';
+
+    document.body.appendChild(button);
+    function scrollToBottom() {
+        window.scrollTo({
+            top: document.documentElement.scrollHeight,
+        });
+    }
+})();
