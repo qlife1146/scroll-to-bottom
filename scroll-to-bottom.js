6e0157b477a768acdac3c88d270cbd973d5e7ebf
Luca Park <qlife1146@gmail.com>
2023-03-15 17:16:23 +0900

    자동 업데이트 변경 및 홈페이지 추가

--- scroll-to-bottom.js
+++ scroll-to-bottom.js
@@ -1,12 +1,15 @@
 // ==UserScript==
-// @name		Scroll to Bottom Button
-// @version		1.0.4
-// @description	페이지 아래로 바로 내려갈 수 있는 버튼을 추가합니다.
-// @author		Luca_Tuna
-// @icon		http://vocaro.wikidot.com/local--files/nav:side/logo.png
-// @downloadURL   https://raw.githubusercontent.com/qlife1146/scroll-to-bottom/master/scroll-to-bottom.js
-// @grant		none
-// @match		http://vocaro.wikidot.com/*
+// @name		    Scroll to Bottom Button
+// @version		    1.0.4151253
+// @description	    페이지 아래로 바로 내려갈 수 있는 버튼을 추가합니다.
+// @author		    Luca_Tuna
+// @icon		    http://vocaro.wikidot.com/local--files/nav:side/logo.png
+// @supportURL      https://github.com/qlife1146/scroll-to-bottom/issues
+// @homepageURL     https://github.com/qlife1146/scroll-to-bottom/
+// @downloadURL     https://raw.githubusercontent.com/qlife1146/scroll-to-bottom/master/scroll-to-bottom.js
+// @updateURL       https://raw.githubusercontent.com/qlife1146/scroll-to-bottom/master/scroll-to-bottom.js
+// @grant		    none
+// @match		    http://vocaro.wikidot.com/*
 // ==/UserScript==
 
 (function () {
